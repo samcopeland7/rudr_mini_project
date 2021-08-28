9b328e244fc45d888db32c5832a6430f17e8d7ef
Sam Copeland <linkherox@gmail.com>
2021-08-28 09:54:12 -0400

    Add methods to determine User's score and update it when new Invite is added

--- app/models/invite.rb
+++ app/models/invite.rb
@@ -1,4 +1,10 @@
 class Invite < ApplicationRecord
+  after_commit :update_associated_user_score
+
   belongs_to :user
   belongs_to :event
+
+  def update_associated_user_score
+    user.update_user_score
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,30 @@
 class User < ApplicationRecord
   has_many :invites
   has_many :events, through: :invites
+
+
+  def update_user_score
+    update(score: user_score)
+  end
+
+  def user_score
+    invites.count + accepted_invites.count - declined_invites.count -
+      (2 * ignored_invites.count) - (3 * non_declined_missed_meetings.count)
+  end
+
+  def accepted_invites
+    invites.where(response: 'accepted')
+  end
+
+  def declined_invites
+    invites.where(response: 'declined')
+  end
+
+  def ignored_invites
+    invites.where(response: 'ignored')
+  end
+
+  def non_declined_missed_meetings
+    invites.where({ attended: false, response: ['accepted', 'ignored'] })
+  end
 end
