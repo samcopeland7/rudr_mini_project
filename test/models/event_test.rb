8845eae0501139ca3a052b1811092bb598485341
Sam Copeland <linkherox@gmail.com>
2021-08-28 14:16:26 -0400

    Tests for Event model

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "an event has an associated list of users" do
+    event = events(:happy_hour)
+    assert event.users.count == 4
+  end
 end
